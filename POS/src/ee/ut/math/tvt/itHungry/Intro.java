d9de84116d4706e48b9a575cdbf1153882f1b038
Abercus <joonas.puura@gmail.com>
2014-10-26 23:14:32 +0200

    Added required library. Organized imports for the application to work with new files.

--- POS/src/ee/ut/math/tvt/itHungry/Intro.java
+++ POS/src/ee/ut/math/tvt/itHungry/Intro.java
@@ -2,16 +2,43 @@ package ee.ut.math.tvt.itHungry;
 
 import org.apache.log4j.Logger;
 
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
-	
+
 	private static final Logger log = Logger.getLogger(Intro.class);
-	
+	private static final String MODE = "console";
+
 	public static void main(String args[]) {
-		final IntroUI userInterface = new IntroUI();
-		userInterface.setResizable(false);
-		userInterface.setVisible(true);
-		
-		log.info("Intro started");
+
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			final IntroUI userInterface = new IntroUI();
+			userInterface.setResizable(false);
+			userInterface.setVisible(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			userInterface.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			userInterface.setVisible(false);
+			log.info("Intro started");
+		}
 	}
 }
